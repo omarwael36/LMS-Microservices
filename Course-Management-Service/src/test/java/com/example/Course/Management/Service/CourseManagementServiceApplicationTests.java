45f18616c769b09c2ccf5f80af45e17a112773ae
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2024-05-10 22:06:31 +0300

    course project added

---
+++ Course-Management-Service/src/test/java/com/example/Course/Management/Service/CourseManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Course.Management.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
