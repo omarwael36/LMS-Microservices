e58f3d8ffea82de4cc88f8834f5fb11b4d2fab10
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2024-05-10 16:56:21 +0300

    project structure and database connection

---
+++ User_Servicer/src/main/java/com/example/user_servicer/MyApplication.java
@@ -0,0 +1,9 @@
+package com.example.user_servicer;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MyApplication extends Application {
+
+}
