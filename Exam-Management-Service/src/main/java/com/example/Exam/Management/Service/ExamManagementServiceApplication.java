e6b3060e39706037a0d5d5329a008bcee7a3f4b1
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2024-05-11 23:32:00 +0300

    Exam management service added to repo

---
+++ Exam-Management-Service/src/main/java/com/example/Exam/Management/Service/ExamManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Exam.Management.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamManagementServiceApplication.class, args);
+	}
+
+}
